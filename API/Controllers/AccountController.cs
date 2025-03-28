9c1d4ab59217bbf99d0ad310b654f6cb927d0ace
Jacky Wu <JackyWuCoder@gmail.com>
2025-03-28 15:29:10 -0700

    feat: compare password hash provided by user and database to check authroization

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,13 +32,23 @@ public class AccountController(DataContext context) : BaseApiController
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDTO)
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x => 
-            x.Username == loginDTO.Username.ToLower());
+            x.Username == loginDto.Username.ToLower());
 
         if (user == null) return Unauthorized("Invalid username");
 
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user;
     }
 
     private async Task<bool> UserExists(string username)
