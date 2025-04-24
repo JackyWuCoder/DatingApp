b2e3322bb186b4d69090e9e0b41f3274b6f7383f
Jacky Wu <JackyWuCoder@gmail.com>
2025-04-24 12:40:59 -0700

    feat(auth): add authentication and authorization middleware to request pipeline

--- API/Program.cs
+++ API/Program.cs
@@ -33,6 +33,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
